c95d6f491e6438810462486df35758867cac6232
Taha Tehrani Nasab <79411820+tahatehran@users.noreply.github.com>
2024-08-06 01:44:40 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    SECRET_KEY = 'your-secret-key-here'  # در یک محیط واقعی، این را در یک متغیر محیطی قرار دهید
-    COINMARKETCAP_API_KEY = 'your-coinmarketcap-api-key-here'  # در یک محیط واقعی، این را در یک متغیر محیطی قرار دهید
+    SECRET_KEY = 'your-secret-key-here'  
+    COINMARKETCAP_API_KEY = 'bb9edc9f-53fe-4709-85d7-c51ade37920e'  
     SQLALCHEMY_DATABASE_URI = 'sqlite:///crypto.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
